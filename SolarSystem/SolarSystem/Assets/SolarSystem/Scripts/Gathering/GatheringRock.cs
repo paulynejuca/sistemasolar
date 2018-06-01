43f38049e902384b035769271b083f3633e96e2d
Gabriel Tavares <gabriel.tavaresramos@gmail.com>
2018-06-01 00:11:15 -0300

    EU ODEIO O GITHUB

---
+++ SolarSystem/SolarSystem/Assets/SolarSystem/Scripts/Gathering/GatheringRock.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatheringRock : GatheringScript {
+
+    #region SERIALIZED FIELDS
+    [SerializeField] private InventoryScript.RockType rockType;             // Tipo de rocha do coletável
+    #endregion
+
+    #region PRIVATE FIELDS
+    private float currentTime = 0f;                                         // Auxiliar para ajudar a contar o tempo
+    private InventoryScript inventory;                                      // Referência do controle de inventário
+
+    private ParticleSystem particle0;
+    private ParticleSystem.EmissionModule emission0;
+    private float rate0;
+
+    private ParticleSystem particle1;
+    private ParticleSystem.EmissionModule emission1;
+    private float rate1;
+    #endregion
+
+    #region OVERRIDE METHOS
+    protected override void Start() {
+        base.Start();
+        inventory = InventoryScript.Instance;
+
+        particle0 = transform.GetChild(0).GetComponent<ParticleSystem>();
+        emission0 = particle0.emission;
+        rate0 = emission0.rateOverTime.constant;
+
+        particle1 = transform.GetChild(1).GetComponent<ParticleSystem>();
+        emission1 = particle1.emission;
+        rate1 = emission1.rateOverTime.constant;
+    }
+
+    protected override void Update() {
+        base.Update();
+    }
+
+    protected override void Gathering() {
+        base.Gathering();
+
+        // Enquanto coleta
+        if (gathering) {
+            if (currentTime <= 1f) {
+                currentTime += Time.deltaTime;
+            } else {
+                amount -= 1;
+                currentTime = 0f;
+                // Reduz o tamanho da nuvem a medida que o jogador coleta
+                emission0.rateOverTime = AmountPercent() * rate0 + .3f;
+                emission1.rateOverTime = AmountPercent() * rate1 + .3f;
+
+                // Se acabou o objeto é desativado
+                if (amount < 0) {
+                    gathering = false;
+                    gameObject.SetActive(false);
+                } else {
+                    // Adiciona ao inventário
+                    inventory.AddRock(rockType, amount);
+                }
+            }
+        }
+    }
+
+    protected override void OnTriggerEnter(Collider other) {
+        base.OnTriggerEnter(other);
+    }
+
+    protected override void OnTriggerExit(Collider other) {
+        base.OnTriggerExit(other);
+    }
+    #endregion
+
+    public InventoryScript.RockType RockType {
+        get { return rockType; }
+        set { rockType = value; }
+    }
+}
---
+++ SolarSystem/SolarSystem/Assets/SolarSystem/Scripts/Spaceship/SpaceshipController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceshipController : MonoBehaviour {
+
+    [SerializeField] private float forwardThrust;                   // Força do impulso
+    [SerializeField] private float turnSpeed;                       // Velocidade de rotação Y
+    [SerializeField] private float rotateSpeed;                     // Velocidade de rotação Z
+
+    private float horizontal = 0f;                                  // Entradas no eixo horizontal
+    private float vertical = 0f;                                    // Entradas no eixo vertical
+    private float thrust = 0f;                                      // Entradas no eixo de impulso
+    private float rotate = 0f;                                      // Entradas no eixo de rotação
+    private bool panic = false;
+
+    // drag posicional
+    [SerializeField] private float sqrdSpeedThresholdForDrag = 25F;
+    [SerializeField] private float fastDrag = 0.5F;
+    [SerializeField] private float slowDrag = 0.01F;
+
+    [SerializeField] private float fuelLevel;                       // Combustível inicial
+    private float fuel;                                             // Combustível atual
+    [SerializeField] private float fuelConsume;                     // Consumo de combustível
+    [SerializeField] private Slider fuelSlider;                     // Elemento de UI que exibe o nível do combustível
+
+    private Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        fuel = fuelLevel;
+        fuelSlider.value = fuelLevel;
+        fuelSlider.maxValue = fuelLevel;
+        fuelSlider.minValue = 0f;
+        fuelSlider.direction = Slider.Direction.RightToLeft;
+    }
+
+    private void Update () {
+        GetInputs();
+        Turn();
+        Rotate();
+	}
+
+    private void FixedUpdate() {
+        Thrust();
+
+        if (Mathf.Abs(thrust) > 0.01f) {
+            if (rb.velocity.sqrMagnitude > sqrdSpeedThresholdForDrag)
+                rb.drag = fastDrag;
+            else
+                rb.drag = slowDrag;
+        }
+
+        if (panic) {
+            rb.velocity = Vector3.Lerp(rb.velocity, rb.velocity * 0.3f, 1 * Time.fixedDeltaTime);
+        }
+    }
+
+    // Pega entradas
+    private void GetInputs() {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        thrust = Input.GetAxis("Thrust");
+        rotate = Input.GetAxis("Rotate");
+        panic = Input.GetKey(KeyCode.CapsLock);
+    }
+
+    private void Thrust() {
+        if (thrust > 0.01f) {
+            rb.AddRelativeForce(Vector3.forward * forwardThrust * Time.fixedDeltaTime);
+            ConsumeFuel();
+        }
+    }
+
+    private void Turn() {
+        if (horizontal > 0.01f)
+            transform.Rotate(Vector3.up * horizontal * turnSpeed * Time.deltaTime);
+
+        if (horizontal < 0.01f)
+            transform.Rotate(Vector3.down * (-horizontal) * turnSpeed * Time.deltaTime);
+
+        if (vertical > 0.01f)
+            transform.Rotate(Vector3.right * (-vertical) * turnSpeed * Time.deltaTime);
+
+        if (vertical < 0.01f)
+            transform.Rotate(Vector3.left * vertical * turnSpeed * Time.deltaTime);
+    }
+
+    private void Rotate() {
+        if (rotate > 0.01f)
+            transform.Rotate(Vector3.forward * (-rotate) * rotateSpeed * Time.deltaTime);
+
+        if (rotate < 0.01f)
+            transform.Rotate(Vector3.back * rotate * rotateSpeed * Time.deltaTime);
+    }
+
+
+    private void ConsumeFuel() {
+        fuel = fuel - (fuelConsume * Time.deltaTime);
+        fuelSlider.value = fuel;
+
+        if (fuel <= 0) {
+            // Game Over
+            Debug.Log("Game Over");
+        }
+    }
+}
